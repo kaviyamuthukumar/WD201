9274aabf16acd8819bfe25cbbdc3e7e00104abd0
kaviyamuthukumar <125263931+kaviyamuthukumar@users.noreply.github.com>
2023-03-15 11:34:36 +0530

    first commit

---
+++ todo-cli/__tests__/todo.js
@@ -0,0 +1,86 @@
+const todoList = require("../todo");  // For Testing
+const { all, markAsComplete, add, overdue, dueLater, dueToday } = todoList();
+
+describe("Todo New Test Suite", () => {
+  beforeAll(() => {
+    add({
+      title: "Test2",
+      completed: false,
+      dueDate: new Date(new Date().setDate(new Date().getDate() - 1))
+        .toISOString()
+        .slice(0, 10),
+    });
+    add({
+      title: "Today fun",
+      completed: false,
+      dueDate: new Date().toISOString().slice(0, 10),
+    });
+    add({
+      title: "Test Due Later",
+      completed: false,
+      dueDate: new Date(new Date().setDate(new Date().getDate() + 1))
+        .toISOString()
+        .slice(0, 10),
+    });
+  });
+  test("add new todo", () => {
+    const store_todo = all.length;
+    add([
+      {
+        title: "Test Over Due",
+        completed: false,
+        dueDate: new Date(new Date().setDate(new Date().getDate() - 1))
+          .toISOString()
+          .slice(0, 10),
+      },
+    ]);
+    expect(all.length).toBe(store_todo + 1);
+  });
+
+  test("Should mark a todo as complete", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+  });
+
+  test("Should check the retreive of Overdue", () => {
+    add({
+      title: "Test Over Due 2",
+      completed: false,
+      dueDate: new Date(new Date().setDate(new Date().getDate() - 1))
+        .toISOString()
+        .slice(0, 10),
+    });
+    expect(
+      all.filter((item) => item.dueDate < new Date().toISOString().slice(0, 10))
+        .length
+    ).toBe(2);
+  });
+
+  test("Should check the retreive of due today", () => {
+    add({
+      title: "Test Due Today",
+      completed: false,
+      dueDate: new Date().toISOString().slice(0, 10),
+    });
+    expect(
+      all.filter(
+        (item) => item.dueDate === new Date().toISOString().slice(0, 10)
+      ).length
+    ).toBe(2);
+  });
+});
+
+test("Overdue item", () => {
+  add({
+    title: "Test 2",
+    completed: false,
+    dueDate: new Date(new Date().setDate(new Date().getDate() + 1))
+      .toISOString()
+      .slice(0, 10),
+  });
+  expect(
+    all.filter((item) => item.dueDate > new Date().toISOString().slice(0, 10))
+      .length
+  ).toBe(2);
+});
